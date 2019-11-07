baab52d964b39cb643c29b98283ac29fd5ca8947
Question Josmyn <qjosmyn@oa-j5.21-school.ru>
2019-11-07 19:31:32 +0300

    kek

--- file.c
+++ file.c
@@ -6,33 +6,43 @@
 /*   By: qjosmyn <qjosmyn@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 18:31:16 by qjosmyn           #+#    #+#             */
-/*   Updated: 2019/11/06 20:24:24 by qjosmyn          ###   ########.fr       */
+/*   Updated: 2019/11/07 19:30:55 by qjosmyn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-#define SIZE 16
-#define SIZE_R 20
-typedef struct	s_tetr
+#define SIZE	16
+#define SIZE_R	20
+#define COORD	8
+typedef struct		s_tetr
 {
+	int				*coordinates;
 	int				*line;
-	int				x;
-	int				y;
 	struct s_tetr	*next;
-}				t_tetr;
+	struct s_tetr	*prev;
+}					t_tetr;
 
-t_tetr 	*ft_newtetr(int len)
+t_tetr 	*ft_newtetr(int len, int crds)
 {
 	t_tetr *ptr;
 
-	if ((ptr = (t_tetr*)malloc(sizeof(t_tetr))) == NULL)
+	ptr = (t_tetr*)malloc(sizeof(t_tetr));
+	if (ptr == NULL)
 		return (NULL);
-	if ((ptr->line = (int*)malloc(sizeof(int) * len)) == NULL)
+	ptr->line = (int*)malloc(sizeof(int) * len);
+	if (ptr->line == NULL)
 		{
 			free(ptr);
 			return (NULL);
 		}
+	ptr->coordinates = (int*)malloc(sizeof(int) * crds);
+	if (ptr->coordinates == NULL)
+	{
+		free(ptr->line);
+		free(ptr);
+		return (NULL);
+	}
 	return (ptr);
 }
 
@@ -45,35 +55,71 @@ int		ft_newstrdel(char **s)
 	return (-1);
 }
 
-int		ft_definition(t_tetr **ptr, int fd, int i, int num)
+int		ft_validsquare(char *tetr)
+{
+	int		i;
+	
+	i = 0;
+	while (i < SIZE)
+	{
+		if (*(tetr++) == '\n' && i % 4 == 0)
+			continue ;
+		if (*(tetr - 1) != '.' && *(tetr - 1) != '#')
+			return (-1);
+		i++;
+	}
+	return (1);
+}
+
+int		ft_binarysquare(char *tetr, t_tetr **ptr)
+{
+	int	i;
+	int	j;
+
+	if (*ptr == NULL)
+		return (-1);
+	i = 0;
+	while (i < SIZE / 4)
+	{
+		j = 0;
+		while (j < SIZE / 4)
+		{
+			(*ptr)->line[i * SIZE / 4 + j] = (*tetr == '.') ? 0 : 1;
+			if (*tetr == '#')
+			{	
+				*((*ptr)->coordinates++) = j;
+				*(*ptr)->coordinates = i;
+			}
+			tetr = (*(tetr + 1) == '\n') ? tetr + 2: tetr + 1;
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int		ft_definition(t_tetr **ptr, int fd)
 {
 	char	*c;
 	char	r;
+	int		num;
 
 	c = ft_strnew(SIZE_R);
-	if (*ptr == NULL)
-		return (-1);
 	if (read(fd, c, SIZE_R) < 0)
-		return (-1);
+		return (ft_newstrdel(&c));
 	c[SIZE_R] = 0;
-	ft_putstr(c);
-	while (i < SIZE)
-	{
-		if (*(c++) == '\n' && i % 4 == 0)
-			continue ;
-		if (*(c - 1) != '.' && *(c - 1) != '#')
-			return (-1);
-		(*ptr)->line[i] = (*(c - 1) == '.') ? 0 : 1;
-		i++;
- 	}
+	if (ft_validsquare(c) == -1)
+		return (ft_newstrdel(&c));
 	num = read(fd, &r, 1);
-	if (r != '\n' && i < SIZE - 1)
-		return (-1);
-	//ft_newstrdel(&c);
+	if (r != '\n')
+		return (ft_newstrdel(&c));
+	if (ft_binarysquare(c, ptr) == -1)
+		return (ft_newstrdel(&c));
+	ft_newstrdel(&c);
 	return (num);
 }
 
-void	ft_putint(int *mas, int len)
+void	ft_putnubrs(int *mas, int len)
 {
 	int i;
 	int j;
@@ -90,14 +136,18 @@ int main(int ac, char **av)
 	int fd = open(av[1], O_RDONLY);
 	t_tetr *ptr = NULL;
 
-	ptr = ft_newtetr(SIZE);
-	ptr->next = ft_newtetr(SIZE);
-	ft_putnbr(ft_definition(&ptr, fd, 0, 0));
+	ptr = ft_newtetr(SIZE, COORD);
+	ptr->next = ft_newtetr(SIZE, COORD);
+	ft_putnbr(ft_definition(&ptr, fd));
 	ft_putchar('\n');
-	ft_putint(ptr->line, SIZE);
-	ft_putnbr(ft_definition(&ptr->next, fd, 0, 0));
+	ft_putnubrs(ptr->line, SIZE);
 	ft_putchar('\n');
-	ft_putint(ptr->next->line, SIZE);
-	close(fd);
+	ft_putnubrs(ptr->coordinates, SIZE / 2);
+	// ft_putnbr(ft_definition(&ptr->next, fd));
+	// ft_putchar('\n');
+	// ft_putnubrs(ptr->next->line, SIZE);
+	// ft_putchar('\n');
+	// ft_putnubrs(ptr->next->coordinates, SIZE);
+	// close(fd);
 	return (1);
 }
